0642362b4312013a9e3256962fa5464142f1900f
Lonnie Coffman <lcoffman@mobilesoft.com>
2020-02-18 14:15:33 -0800

    Get new access token using refresh token

--- v1/controller/session.php
+++ v1/controller/session.php
@@ -16,7 +16,7 @@ catch (PDOException $e) {
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method === 'POST') handle_post($write_db);
 else if ($method === 'DELETE') handle_delete($write_db);
-else if ($method === 'PATCH') handle_delete($write_db);
+else if ($method === 'PATCH') handle_patch($write_db);
 else returnErrorResponse(405, 'Request method not allowed');
 
 /*
@@ -100,9 +100,9 @@ function handle_post($db) {
     }
 
     // create access token and refresh token - add unix time to end to eliminate chance of stale token usage
-    $access_token = base64_encode(bin2hex(openssl_random_pseudo_bytes(24))).time();
+    $access_token = base64_encode(bin2hex(openssl_random_pseudo_bytes(24).time()));
     $access_token_expiry_seconds = 60 * 60; // 1 hour
-    $refresh_token = base64_encode(bin2hex(openssl_random_pseudo_bytes(24))).time();
+    $refresh_token = base64_encode(bin2hex(openssl_random_pseudo_bytes(24).time()));
     $refresh_token_expiry_seconds = 60 * 60 * 24 * 7 * 2; // 2 weeks
     
   }
@@ -163,9 +163,132 @@ function handle_post($db) {
 /*
  * HANDLE PATCH REQUEST - REFRESH TOKEN
  */
-function handle_patch() {
+function handle_patch($db) {
   if (array_key_exists('sessionid', $_GET)) {
-    
+
+    // header must be json
+    if ($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+      returnErrorResponse(400, 'Content type header is not set to JSON');
+    }
+
+    // body must be json
+    $raw_post_data = file_get_contents('php://input');
+    if (!$json_data = json_decode($raw_post_data)) {
+      returnErrorResponse(400, 'Request body is not valid JSON');
+    }
+
+    // get session id
+    $session_id = $_GET['sessionid'];
+    if ($session_id === '') returnErrorResponse(400, 'Session ID cannot be blank');
+    if (!is_numeric($session_id)) returnErrorResponse(400, 'Session ID must be numeric');
+
+    // get authentication header
+    if (!isset($_SERVER['HTTP_AUTHORIZATION'])) returnErrorResponse(401, 'Access token is missing from the header');
+    if (strlen($_SERVER['HTTP_AUTHORIZATION']) < 1) returnErrorResponse(401, 'Access token cannot be blank');
+
+    $access_token = $_SERVER['HTTP_AUTHORIZATION'];
+
+    // refresh token provided
+    if (!isset($json_data->refresh_token)) returnErrorResponse(400, 'Refresh token not supplied');
+    if (strlen($json_data->refresh_token) < 1) returnErrorResponse(400, 'Refresh token cannot be blank');
+
+    try {
+
+      $refresh_token = $json_data->refresh_token;
+
+      $query = $db->prepare('
+        SELECT
+          s.id as session_id,
+          s.userid as user_id,
+          s.accesstoken,
+          s.accesstokenexpiry,
+          s.refreshtoken,
+          s.refreshtokenexpiry,
+          u.useractive,
+          u.loginattempts
+        FROM
+          sessions as s,
+          users as u
+        WHERE u.id = s.userid
+        AND s.id = :session_id
+        AND s.accesstoken = :access_token
+        AND s.refreshtoken = :refresh_token
+      ');
+      $query->execute([$session_id, $access_token, $refresh_token]);
+
+      $row_count = $query->rowCount();
+
+      if ($row_count === 0) {
+        returnErrorResponse(401, 'Access token or refresh token is incorrect for session id');
+      }
+
+      $row = $query->fetch(PDO::FETCH_ASSOC);
+
+      $returned_session_id = $row['session_id'];
+      $returned_user_id = $row['user_id'];
+      $returned_access_token = $row['accesstoken'];
+      $returned_access_token_expiry = $row['accesstokenexpiry'];
+      $returned_refresh_token = $row['refreshtoken'];
+      $returned_refresh_token_expiry = $row['refreshtokenexpiry'];
+      $returned_useractive = $row['useractive'];
+      $returned_loginattempts = $row['loginattempts'];
+
+      if ($returned_useractive !== 'Y') returnErrorResponse(401, 'User account is not active');
+      if ($returned_loginattempts >= 3) returnErrorResponse(401, 'User account is currently locked out');
+
+      // refresh token expired
+      if (strtotime($returned_refresh_token_expiry) < time()) returnErrorResponse(401, 'Refresh token has expired - please log in again');
+
+      // create access token and refresh token - add unix time to end to eliminate chance of stale token usage
+      $access_token = base64_encode(bin2hex(openssl_random_pseudo_bytes(24).time()));
+      $access_token_expiry_seconds = 60 * 60; // 1 hour
+      $refresh_token = base64_encode(bin2hex(openssl_random_pseudo_bytes(24).time()));
+      $refresh_token_expiry_seconds = 60 * 60 * 24 * 7 * 2; // 2 weeks
+
+      $query = $db->prepare('
+        UPDATE sessions
+        SET accesstoken = :access_token,
+            accesstokenexpiry = DATE_ADD(NOW(), INTERVAL :access_token_expiry_seconds SECOND),
+            refreshtoken = :refresh_token,
+            refreshtokenexpiry = DATE_ADD(NOW(), INTERVAL :refresh_token_expiry_seconds SECOND)
+        WHERE id = :returned_session_id
+        AND userid = :returned_user_id
+        AND accesstoken = :returned_access_token
+        AND refreshtoken = :returned_refresh_token
+      ');
+      $query->execute([
+        $access_token,
+        $access_token_expiry_seconds,
+        $refresh_token,
+        $refresh_token_expiry_seconds,
+        $returned_session_id,
+        $returned_user_id,
+        $returned_access_token,
+        $returned_refresh_token
+      ]);
+
+      $row_count = $query->rowCount();
+
+      if ($row_count === 0) returnErrorResponse(401, 'Access token could not be refreshed - please log in again');
+
+      $return_data = [];
+      $return_data['session_id'] = intval($returned_session_id);
+      $return_data['access_token'] = $access_token;
+      $return_data['access_token_expires_in'] = $access_token_expiry_seconds;
+      $return_data['refresh_token'] = $refresh_token;
+      $return_data['refresh_token_expires_in'] = $refresh_token_expiry_seconds;
+
+      $response = new Response();
+      $response->setHttpStatusCode(200);
+      $response->setSuccess(true);
+      $response->addMessage('Token refreshed');
+      $response->setData($return_data);
+      $response->send();
+      exit;
+    }
+    catch (PDOException $e) {
+      returnErrorResponse(500, 'There was an issue refreshing access token - please log in again');
+    }
   }
   else {
     returnErrorResponse(404, 'Endpoint not found');
@@ -175,9 +298,46 @@ function handle_patch() {
 /*
  * HANDLE DELETE REQUEST - LOGOUT
  */
-function handle_delete() {
+function handle_delete($db) {
   if (array_key_exists('sessionid', $_GET)) {
-    
+    $session_id = $_GET['sessionid'];
+
+    if ($session_id === '') returnErrorResponse(400, 'Session ID cannot be blank');
+    if (!is_numeric($session_id)) returnErrorResponse(400, 'Session ID must be numeric');
+
+    // get authentication header
+    if (!isset($_SERVER['HTTP_AUTHORIZATION'])) returnErrorResponse(401, 'Access token is missing from the header');
+    if (strlen($_SERVER['HTTP_AUTHORIZATION']) < 1) returnErrorResponse(401, 'Access token cannot be blank');
+
+    $access_token = $_SERVER['HTTP_AUTHORIZATION'];
+
+    try {
+      $query = $db->prepare('
+        DELETE FROM sessions WHERE id = :session_id AND accesstoken = :access_token
+      ');
+      $query->execute([$session_id, $access_token]);
+
+      $row_count = $query->rowCount();
+
+      if ($row_count === 0) {
+        returnErrorResponse(400, 'Failed to log out of this session using this access token');
+      }
+
+      $return_data = [];
+      $return_data['session_id'] = intval($session_id);
+  
+      $response = new Response();
+      $response->setHttpStatusCode(200);
+      $response->setSuccess(true);
+      $response->addMessage('Logged out');
+      $response->setData($return_data);
+      $response->send();
+      exit;
+    }
+    catch (PDOException $e) {
+      $db->rollBack();
+      returnErrorResponse(500, 'There was an issue logging out - please try again');
+    }
   }
   else {
     returnErrorResponse(404, 'Endpoint not found');
